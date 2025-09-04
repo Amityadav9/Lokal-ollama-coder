a3f55dccc64420eb70e52d892381a020c0b0f13b
Amit Yadav <67372926+Amityadav9@users.noreply.github.com>
2025-09-04 17:25:13 +0200

    Update __init__.py

--- mit_rag/scr/__init__.py
+++ mit_rag/scr/__init__.py
@@ -1,1 +1,19 @@
+"""src package exports for rag module"""
 
+from .rag import (
+    test_ollama_connection,
+    process_and_initialize,
+    user_query_typing_effect,
+    initialize_chatbot,
+    load_doc,
+    create_db,
+)
+
+__all__ = [
+    "test_ollama_connection",
+    "process_and_initialize",
+    "user_query_typing_effect",
+    "initialize_chatbot",
+    "load_doc",
+    "create_db",
+]
